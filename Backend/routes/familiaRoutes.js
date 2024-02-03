7468b659279643ac87ded08ccfd83bbbe8bef61d
Jaime Rafael Fraile Bautista <119857152+Sp3cTRe02@users.noreply.github.com>
2024-02-03 01:50:28 +0100

    crud de familias

--- Backend/routes/familiaRoutes.js
+++ Backend/routes/familiaRoutes.js
@@ -1,6 +1,17 @@
 const { Router } = require('express')
 const router = Router()
-const { StatusCodes } = require('http-status-codes')
+const { check } = require('express-validator')
+const validarCampos = require('../middlewares/validarCampos')
+const controlador = require('../controllers/familiaController')
 
+const validator = [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    validarCampos
+]
+
+router.get('/familia', controlador.familiaGet)
+router.post('/familia', validator, controlador.familiaPost)
+router.put('/familia/:id', controlador.familiaPut)
+router.delete('/familia/:id', controlador.familiaDelete)
 
 module.exports = router
