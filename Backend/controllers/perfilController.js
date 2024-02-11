5c8b16e128c85b4cc62ea3d01a3579be7682163f
Ismael <145430109+ismasarry@users.noreply.github.com>
2024-02-11 18:15:14 +0100

    hago el metodo update del usuario en el controlador

--- Backend/controllers/perfilController.js
+++ Backend/controllers/perfilController.js
@@ -22,6 +22,29 @@ const getUsuarioPorId = async (req = request, res= response) => {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error interno del servidor' });
   }
 };
+
+const updateUsuarioPorId = async (req = request, res = response) => {
+  try {
+    const { id } = req.params;
+    const { body } = req;
+    const usuario = await Conexion.actualizarUsuarioPorId(id, body);
+
+    if (usuario === 0) {
+      return res.status(StatusCodes.NOT_FOUND).json({ message: 'Usuario no encontrado' });
+    } else {
+      return res.status(StatusCodes.OK).json({ message: 'Usuario actualizado con éxito' });
+    }
+  } catch (error) {
+    console.error('Error al actualizar el usuario:', error);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'Error interno del servidor' });
+  }
+};
+
+
+
+
+
 module.exports = {
- getUsuarioPorId
+ getUsuarioPorId,
+ updateUsuarioPorId
 }
