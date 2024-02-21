2372ff864e4f775380eea8e62cb6d84b6f4bea92
Ismael <145430109+ismasarry@users.noreply.github.com>
2024-02-21 12:57:17 +0100

    controlador del mail

---
+++ Backend/controllers/mailController.js
@@ -0,0 +1,40 @@
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+
+    type: 'OAuth2',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.MAIL_CLIENT_ID,
+    clientSecret: process.env.MAIL_CLIENT_SECRET,
+    refreshToken: process.env.MAIL_REFRESH_TOKEN,
+    accessToken: process.env.MAIL_ACCESS_TOKEN
+    }
+});
+
+let mailOptions = {
+    from: process.env.MAIL_USER,
+    to: 'ismael22012004@gmail.com', //parametrizar con llamada a la base de datos con al gmail introducido y buscar su contraseña para mandarla
+    subject: 'Prueba de Nodemailer',
+    text: 'Esto es una prueba de Nodemailer',
+}
+
+const enviarCorreo = (req, res) => {
+    
+    transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+    console.log ( 'Error al enviar el correo electronico:', error);
+    res.status(203).json({'msg' : 'Correo NO enviado'})
+    } else {
+        console.log ( 'correo electronico enviado correctamente:', info.response);
+    res.status(200).json({'msg' : 'Correo enviado'})
+    }
+
+    });
+}
+module.exports ={
+enviarCorreo,
+
+}
