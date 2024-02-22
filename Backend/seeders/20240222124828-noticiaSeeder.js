6bf910b3e3ee16f5348dcbaa761e2dc7f43f71a2
davitru60 <davitru60@gmail.com>
2024-02-22 21:28:54 +0100

    cambios

---
+++ Backend/seeders/20240222124828-noticiaSeeder.js
@@ -0,0 +1,14 @@
+'use strict';
+const noticiaFactory = require('../factories/noticiaFactory')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const noticias = await noticiaFactory(4)
+    await queryInterface.bulkInsert('contenidos',noticias,{})
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('contenidos', null, {});
+  }
+};
