560580d543745479c969a3a7cca36fd475c647b5
Jaime Rafael Fraile Bautista <119857152+Sp3cTRe02@users.noreply.github.com>
2024-02-23 19:23:55 +0100

    token en el session e interceptor recoge token

--- Frontend/src/app/shared/interceptor/auth.interceptor.ts
+++ Frontend/src/app/shared/interceptor/auth.interceptor.ts
@@ -9,7 +9,7 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const param = req.params.get('auth')
   if(param){
 
-    const token = localStorage.getItem('token')!
+    const token = sessionStorage.getItem('token')!
     clonPeticion = req.clone({
       headers : req.headers.set('x-token', token)
     })
