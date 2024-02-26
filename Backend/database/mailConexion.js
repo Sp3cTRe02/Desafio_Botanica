d17e982201170a06341b44585dd8dea7d6dc1653
Ismael <145430109+ismasarry@users.noreply.github.com>
2024-02-26 11:04:57 +0100

    añadir transporter y rutas

--- Backend/database/mailConexion.js
+++ Backend/database/mailConexion.js
@@ -1,3 +1,21 @@
+const nodemailer = require('nodemailer');
+
 /**
  * @author @Ismael
  */
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+
+    type: 'OAuth2',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASSWORD,
+    clientId: process.env.MAIL_CLIENT_ID,
+    clientSecret: process.env.MAIL_CLIENT_SECRET,
+    refreshToken: process.env.MAIL_REFRESH_TOKEN,
+    accessToken: process.env.MAIL_ACCESS_TOKEN
+    }
+});
+
+module.exports = transporter
--- Backend/routes/mailRoutes.js
+++ Backend/routes/mailRoutes.js
@@ -3,7 +3,22 @@ const { Router } = require('express');
 const router = Router()
 const { StatusCodes } = require('http-status-codes')
 const mailController = require('../controllers/mailController')
+const { mandarEmailRecuperacion } = require('../controllers/mailController');
 
 router.get ('/', mailController.enviarCorreo)
+
+router.post('/recuperarContraseña', async (req, res) => {
+    const { email } = req.body;
+
+    const contraseña = 'ContraseñaTemporal123'; 
+
+    try {
+        await sendRecoveryEmail(email, contraseña);
+        res.status(200).send('Correo de recuperación enviado');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al enviar el correo de recuperación');
+    }
+});
 
 module. exports = router
