5aec8d57092e892365a7afac8d6bebfb16b1a20f
Ismael <145430109+ismasarry@users.noreply.github.com>
2024-02-26 11:04:37 +0100

    parametrizar usuarios

--- Backend/controllers/mailController.js
+++ Backend/controllers/mailController.js
@@ -1,43 +1,65 @@
 /**
  * @author @Ismael
  */
-const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
+const transporter = require('../database/mailConexion');
 
-    type: 'OAuth2',
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASSWORD,
-    clientId: process.env.MAIL_CLIENT_ID,
-    clientSecret: process.env.MAIL_CLIENT_SECRET,
-    refreshToken: process.env.MAIL_REFRESH_TOKEN,
-    accessToken: process.env.MAIL_ACCESS_TOKEN
-    }
-});
+// let transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
 
-let mailOptions = {
-    from: process.env.MAIL_USER,
-    to: 'ismael22012004@gmail.com', //parametrizar con llamada a la base de datos con al gmail introducido y buscar su contraseña para mandarla
-    subject: 'Nueva contraseña',
-    text: 'Esto es una prueba de Nodemailer', //contraseña nueva generada automaticamente y cambiada en la base de datos
-}
+//     type: 'OAuth2',
+//     user: process.env.MAIL_USER,
+//     pass: process.env.MAIL_PASSWORD,
+//     clientId: process.env.MAIL_CLIENT_ID,
+//     clientSecret: process.env.MAIL_CLIENT_SECRET,
+//     refreshToken: process.env.MAIL_REFRESH_TOKEN,
+//     accessToken: process.env.MAIL_ACCESS_TOKEN
+//     }
+// });
 
-const enviarCorreo = (req, res) => {
+// let mailOptions = {
+//     from: process.env.MAIL_USER,
+//     to: 'ismael22012004@gmail.com', //parametrizar con llamada a la base de datos con al gmail introducido y buscar su contraseña para mandarla
+//     subject: 'Nueva contraseña',
+//     text: 'Esto es una prueba de Nodemailer', //contraseña nueva generada automaticamente y cambiada en la base de datos
+// }
+
+// const enviarCorreo = (req, res) => {
     
-    transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-    console.log ( 'Error al enviar el correo electronico:', error);
-    res.status(203).json({'msg' : 'Correo NO enviado'})
-    } else {
-        console.log ( 'correo electronico enviado correctamente:', info.response);
-    res.status(200).json({'msg' : 'Correo enviado'})
+//     transporter.sendMail(mailOptions, function(error, info) {
+//     if (error) {
+//     console.log ( 'Error al enviar el correo electronico:', error);
+//     res.status(203).json({'msg' : 'Correo NO enviado'})
+//     } else {
+//         console.log ( 'correo electronico enviado correctamente:', info.response);
+//     res.status(200).json({'msg' : 'Correo enviado'})
+//     }
+
+//     });
+// }
+
+
+
+
+const mandarEmailRecuperacion = async (email, contraseña) => {
+    const mailOptions = {
+        from: 'ismael22012004@gmail.com',
+        to: email,
+        subject: 'Recuperación de Contraseña',
+        text: `Hola, tu contraseña es: ${contraseña}`
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log('Correo de recuperación enviado');
+    } catch (error) {
+        console.error('Error al enviar el correo:', error);
     }
+};
 
-    });
-}
-module.exports ={
-enviarCorreo,
+module.exports = {
+    mandarEmailRecuperacion,/*enviarCorreo*/
+};
 
-}
+
