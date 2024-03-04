96c016e09c84bc9ca6766292302f6561a725070f
davitru60 <davitru60@gmail.com>
2024-03-04 20:03:50 +0100

    pruebas

--- Backend/routes/arbolesRoutes.js
+++ Backend/routes/arbolesRoutes.js
@@ -6,7 +6,6 @@ const arboles = require ('../controllers/arbolesController')
  * @David_Trujillo
  */
 
-
 router.get('/lista-arboles', arboles.getArbolesGeneral)
 router.get('/:id',arboles.getInformacionArbol)
 router.get('/ubi/:id',arboles.getUbicacionesArbol)
---
+++ Backend/tests/eventosRoutes.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+const express = require('express');
+const routes = require('../routes/contenidoRoutes')
+
+const app = express()
+app.use(express.json());
+app.use('/', routes);
+
+describe('Test para los metodos get', ()=>{
+    test('Debe responder al método GET en / y responder un codigo 200', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(200);
+    });
+
+    test('Debe responder al método GET en /:id y responder un codigo 200 y success true', async () => {
+        const response = await request(app).get('/1');
+        expect(response.statusCode).toBe(200);
+        expect(response.body.success).toBe(true)
+    });
+
+    test('Debe responder al método GET en /:id y responder un codigo 500 y success false', async () => {
+        const response = await request(app).get('/0');
+        expect(response.statusCode).toBe(500);
+    });
+
+    test('Debe responder al método GET en /plazas/:id y responder un codigo 404 y success false', async () => {
+        const response = await request(app).get('/plazas/0');
+        expect(response.statusCode).toBe(404);
+    });
+
+})
